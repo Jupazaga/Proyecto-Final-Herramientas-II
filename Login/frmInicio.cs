7a9fdd05e2744e3c305f0957e13b39e372940952
Pablo <zapatagallegojuan@gmail.com>
2022-11-01 08:10:38 -0500

    Adelanto de conexion con el servidor local de SQL

--- Login/frmInicio.cs
+++ Login/frmInicio.cs
@@ -6,11 +6,16 @@ namespace Login
         {
             InitializeComponent();
         }
-        SqlConnection conexion = new SqlConnection();
+        SqlConnection conexion = new SqlConnection("server=DESKTOP-D481L0S\\SQLEXPRESS; database=DB_Biblioteca2019; integrated security =true");
 
         private void btnValidar_Click(object sender, EventArgs e)
         {
-            
+            conexion.Open();
+            string consulta = $"select ";
+            SqlCommand sqlCommand = new SqlCommand(consulta, conexion);
+            sqlCommand.ExecuteNonQuery();
+            MessageBox.Show("Consulta hecha");
+            conexion.Close();
         }
     }
 }
